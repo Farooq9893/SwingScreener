8586d5d8fd2c8028e043b8aa8925eedc6b994051
Farooq9893 <farooq9893@gmail.com>
2025-11-05 21:19:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import streamlit as st
-from google.oauth2 import service_account
-import gspread
 import pandas as pd
+import gspread
+from google.oauth2 import service_account
 
-# Load credentials from Streamlit Secrets
+st.title("Swing Screener")
+
+# Load credentials safely from Streamlit secrets
 creds = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"],
 )
+
 client = gspread.authorize(creds)
-sheet = client.open("streamlit_service").sheet1
+
+# Open your sheet
+sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1eyLEPy91_bS4d5wD5DF7IJvZIqhtv55i7ijqncWTNL0/edit?usp=sharing").sheet1
+
 data = sheet.get_all_records()
 df = pd.DataFrame(data)
 st.dataframe(df)
